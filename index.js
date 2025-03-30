459a7e3c4b8a039e211e643d5f073a55e12f1e3b
Полина <zeganovapolina@gmail.com>
2025-03-30 19:25:15 +0400

    Исправила баг с обработчиками

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import {renderComments} from "./modules/renderComments.js";
 import {initLikeListeners,initButtonListener,initCommentClickListeners} from "./modules/initListeners.js";
 
 renderComments();
-initLikeListeners();
 initButtonListener();
-initCommentClickListeners();
